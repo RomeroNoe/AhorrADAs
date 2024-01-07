1796985c277e72523fa1d84f532fc04ac584b494
IrenaMartty <irenamartincevic3@gmail.com>
2024-01-07 14:57:35 +0100

    Funcion de borrar la operacion; tailwind para la categorias y reportes; event listener de add new category button

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,8 +1,7 @@
-/* UTILIDADES */
+/* UTILITIES */
 
 const $ = (selector) => document.querySelector(selector)
 const $$ = (selector) => document.querySelectorAll(selector)
-
 
 const randomId = () => self.crypto.randomUUID()
 
@@ -20,29 +19,8 @@ const hideElement = (selectors) => {
 const cleanContainer = (selector) => $(selector).innerHTML = ""
 
 /*LOCAL STORAGE */
-// const getData = (key) => JSON.parse(localStorage.getItem(key)) //// Irena: a mi me sale error aqui: localStorage is not defined
-// const setData = (key, data) => localStorage.setItem(key, JSON.stringify(data))
-
-// const allOperations = getData("operations") || [] //// Irena: a mi me sale error aqui: localStorage is not defined
-
-
-// Irena: he encontrado una solucion para el localStorage, pero es diferente de lo que hemos hecho en la clase:
-const getData = (key) => {
-    try {
-        return JSON.parse(localStorage.getItem(key)) || [];
-    } catch (error) {
-        console.error('Error reading from localStorage:', error);
-        return [];
-    }
-};
-
-const setData = (key, data) => {
-    try {
-        localStorage.setItem(key, JSON.stringify(data));
-    } catch (error) {
-        console.error('Error writing to localStorage:', error);
-    }
-};
+const getData = (key) => JSON.parse(localStorage.getItem(key))
+const setData = (key, data) => localStorage.setItem(key, JSON.stringify(data))
 
 
 //Date
@@ -61,6 +39,120 @@ const setData = (key, data) => {
 
 // const firstDayOfTheMonth = new Date(today.getFullYear(), today.getMonth(), 1)
 // $("#since-filter").valueAsDate = firstDayOfTheMonth  //Label Desde / Filtros, es para que aparezca el dia actual cuando se abre el almanaque
+
+/* RENDERS*/
+
+const renderOperations = (operations) => {
+    for (const operation of operations) {
+        $(".tbody-info-render").innerHTML += `
+        <tr>
+            <td>${operation.description}</td>
+            <td>${operation.category}</td>
+            <td>${operation.day}</td>
+            <td>${operation.amount}</td>
+            <td>
+                <button class="btn btn-edit text-teal-500 hover:text-black" onclick="editForm('${operation.id}')">Editar</i></button>
+                <button type="button" class="btn btn-remove text-teal-500 hover:text-black" onclick="ejecutionDeleteBtn('${operation.id}','${operation.description}')" data-bs-toggle="modal" data-bs-target="#delete-modal">Eliminar</i></button>
+            </td>
+        </tr>
+        `
+    }
+}
+
+
+/* Data handlers */
+const operationsPlaceholder = []
+const categoriesPlaceholder = []
+
+
+const saveOperation = () => {
+    return{
+        id: randomId(),
+        description: $("#input-description-text").value,
+        category: $("#select-category").value,
+        day: $("#op-input-date").value,
+        amount: $("#input-amount").valueAsNumber,
+        type: $("#select-type").value,
+    }
+}
+
+
+//Operations
+
+const getOperationById = (operationId) => getOperations().find(({id}) => id === operationId)
+/* Add new operation */
+
+const showOperations = (arrayOperations) => {
+    $(".tbody-info-render").innerHTML = "" 
+        if (!(arrayOperations.length > 0)) {
+            $(".section-operaciones").classList.remove("hidden");
+            $(".div-table-container").classList.add("hidden");
+         }
+
+    const categoryName = (idCategory) => {
+        for(const category of getData("categories")){
+          if(idCategory === category.id){
+            return(category.category)
+          }
+        }
+      }
+
+    for (const operation of arrayOperations) {    
+        $(".tbody-info-render").innerHTML +=
+
+   `<tr>
+        <td class="text-center border-r-6 p-3max-w-[150px]">${operation.description}</td>
+        <td class="text-center border-r-6 p-3">
+            <p class="bg-slate-300 text-center rounded-md">${categoryName(operation.categoria)}</p>
+         </td>
+        <td class="text-center border-r-6 p-3">${operation.day}</td>
+        <td class="text-center border-r-6 p-3" id="num-amount">${operation.amount}</td>
+        <td class="p-3 flex flex-col">
+            <button class="bg-slate-300 text-center mb-1 border-r-6 rounded-md" onclick="ejecutionOfNewOp('${operation.id}')">Editar</button>
+            <button class="bg-slate-300 text-center border-r-6 rounded-md" onclick="ejecutionDeleteBtn('${operation.id}', '${operation.description}')">Eliminar</button>
+        </td>
+    </tr>
+    <tr class="m-28 border-2 border-slate-300"></tr> 
+    `
+  }
+}
+
+// Delete operation
+
+const ejecutionDeleteBtn = (operationId, operationDescription) => {
+    $(".btn-remove").setAttribute("data-id", operationId);
+    $(".description").innerText = `${operationDescription}`;
+    $(".btn-remove").addEventListener("click", () => {
+        const operationId = $(".btn-remove").getAttribute("data-id")
+        deleteOperation(operationId);
+        showOperations(getData("operations"));
+
+    });
+}
+
+const deleteOperation = (operationId) => {
+    const currentData = getData("operations").filter(operation => operation.id != operationId);
+    setData("operations", currentData);
+    window.location.reload()
+
+}
+
+// Edit Form
+
+const editForm = (operationId) => {
+    hideElement([".section-filtros-balance-operaciones", "#btn-add-newOp"])
+    showElement([".section-newOperation", ".btn-confirm-edit"])
+    $(".btn-confirm-edit").setAttribute("data-id", operationId)
+    const operationEdit = getData("operations").find(operation => operation.id === operationId)
+    $("#input-description-text").value = operationEdit.description
+    $("#select-category").value = operationEdit.category
+    $("#op-input-date").value = operationEdit.day
+    $("#input-amount").value = operationEdit.amount
+    $("#select-type").value = operationEdit.type
+
+}
+
+
 
 
 //Categories
@@ -85,10 +177,9 @@ const getCategoryNameById = (categoryId) => {
     return categorySelected ? categorySelected.name : ''
 }
 
-
 //Add  New Category
 
-const addNewCategory = () => {
+const addNewCategory = (categories) => {
     const categoryName = $("#input-add").value
     if (categoryName) {
         const newCategory = {
@@ -118,7 +209,6 @@ const deleteCategory = (categoryId) => {
 
 //Edit Category
 
-
 const viewEditCategory = (categoryId) => {
     hiddenElement(["#section-newOperation", "#section-reports", "#section-categories", "#section-balance"])
     $("#edit-category-section").classList.remove('hidden')
@@ -142,61 +232,6 @@ const confirmEditCategory = () => {
 }
 
 
-
-//Operations
-
-const getOperationById = (operationId) => getOperations().find(({id}) => id === operationId)
-
-
-/* RENDERS*/
-
-const renderOperations = (operations) => {
-    for (const operation of operations) {
-        $(".tbody-info-render").innerHTML += `
-        <tr>
-            <td>${operation.description}</td>
-            <td>${operation.category}</td>
-            <td>${operation.day}</td>
-            <td>${operation.amount}</td>
-            <td>
-                <button class="btn btn-edit" onclick="editForm('${operation.id}')">Editar</i></button>
-                <button type="button" class="btn btn-remove" data-bs-toggle="modal" data-bs-target="#delete-modal">Eliminar</i></button>
-            </td>
-        </tr>
-        `
-    }
-}
-
-
-/* Data handlers */
-const operationsPlaceholder = []
-
-const saveOperation = () => {
-    return{
-        id: randomId(),
-        description: $("#input-description-text").value,
-        category: $("#select-category").value,
-        day: $("#op-input-date").value,
-        amount: $("#input-amount").valueAsNumber,
-        type: $("#select-type").value,
-    }
-}
-//cambié nombres a inglés, y agregué el type
-
-const editForm = (operationId) => {
-    hideElement([".section-filtros-balance-operaciones", "#btn-add-newOp"])
-    showElement([".section-newOperation", ".btn-confirm-edit"])
-    $(".btn-confirm-edit").setAttribute("data-id", operationId)
-    const operationEdit = getData("operations").find(operation => operation.id === operationId)
-    $("#input-description-text").value = operationEdit.description
-    $("#select-category").value = operationEdit.category
-    $("#op-input-date").value = operationEdit.day
-    $("#input-amount").value = operationEdit.amount
-    $("#select-type").value = operationEdit.type
-
-}
-
-
 /*BALANCE*/
 
 const balanceCostProfit = (array, tipo) => {
@@ -209,8 +244,8 @@ const balanceCostProfit = (array, tipo) => {
     }, 0)
     return spent
 }
-
-const totalBalance = balanceCostProfit(operations, "revenue") - balanceCostProfit(operations, "spent")
+// Irena: Lo comente porque estaba rompiendo la funcion de los botones
+// const totalBalance = balanceCostProfit(operations, "revenue") - balanceCostProfit(operations, "spent")
 
 const updatedBalance = () => {
     $("#total-profit").innerHTML = `+$${balanceCostProfit(operations, "revenue")}`
@@ -247,7 +282,9 @@ const calculateBalance = (transactions) => {
 
 const initializeApp = () => {
     setData("operations", allOperations) 
+    setData("categories", allCategories)
     renderOperations(allOperations) 
+    addNewCategory(allCategories)
  
     // Navigation 
 
@@ -285,64 +322,37 @@ const initializeApp = () => {
         setData("operations", currentData)
         hideElement([".section-newOperation"])
         showElement([".section-filtros-balance-operaciones"])
+        window.location.reload()
     })
 
-    // aqui me sale error: Uncaught ReferenceError: operation is not defined
     $(".btn-confirm-edit").addEventListener("click", (e) => {
         e.preventDefault()
         const operationId = $(".btn-confirm-edit").getAttribute("data-id")
-        const currentData = getData("operations").map( user => {
+        const currentData = getData("operations").map( operation => {
             if(operation.id === operationId) {
                 return saveOperation(operationId)
             }
             return operation
         })
         setData("operations", currentData )
+        window.location.reload()
 
     })
+
+    $("#btn-add-categories").addEventListener("click", (e) => {
+        e.preventDefault()
+        addNewCategory();
+    })
+
+
 
 
 }
 
 window.addEventListener("load", initializeApp)
 
-/*NUEVA OPERACION */
 
-const showOperations = (arrayOperations) => {
-    $(".tbody-info-render").innerHTML = "" 
-        if (!(arrayOperations.length > 0)) {
-            $(".section-operaciones").classList.remove("hidden");
-            $(".div-table-container").classList.add("hidden");
-         }
 
-    const categoryName = (idCategory) => {
-        for(const category of getData("categories")){
-          if(idCategory === category.id){
-            return(category.category)
-          }
-        }
-      }
-
-    for (const operation of arrayOperations) {    
-        $(".tbody-info-render").innerHTML +=
-
-//HAY QUE ACOMODARLO *llora en tailwind*
-   `<tr>
-        <td class="text-center border-r-6 p-3max-w-[150px]">${operation.descripcion}</td>
-        <td class="text-center border-r-6 p-3">
-            <p class="bg-slate-300 text-center rounded-md">${categoryName(operation.categoria)}</p>
-         </td>
-        <td class="text-center border-r-6 p-3">${operation.day}</td>
-        <td class="text-center border-r-6 p-3" id="num-amount">${operation.amount}</td>
-        <td class="p-3 flex flex-col">
-            <button class="bg-slate-300 text-center mb-1 border-r-6 rounded-md" onclick="ejecutionOfNewOp('${operation.id}')">Editar</button>
-            <button class="bg-slate-300 text-center border-r-6 rounded-md" onclick="ejecutionDeleteBtn('${operation.id}', '${operation.descripcion}')">Eliminar</button>
-        </td>
-    </tr>
-    <tr class="m-28 border-2 border-slate-300"></tr> 
-    `
-  }
-}
 //Reports
 
 // Total de ganancias, gastos y balance por mes
